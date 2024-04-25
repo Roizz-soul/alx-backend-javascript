ed2a0dc8a5d9d1993a90b5d51d9f663d7b7754ce
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-04-25 10:18:57 +0000

    task 5

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/extensions */
+
+export default function uploadPhoto(filename) {
+  return new Promise((reject) => {
+    const error = new Error(`${filename} cannot be processed`);
+    reject(error);
+  });
+}
