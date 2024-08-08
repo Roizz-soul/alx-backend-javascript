87862cd89dd05165a7b12e85c07dba4a007ca4c5
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-08-08 14:38:59 +0000

    Task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,56 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < students.length; i++) {
+        if (typeof students[i] !== 'string') {
+          throw new TypeError('Students must be an array of strings');
+        }
+      }
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') { throw new TypeError('Name must be a string'); }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') { throw new TypeError('Length must be a number'); }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) { throw new TypeError('Students must be an array'); }
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < newStudents.length; i++) {
+      if (typeof newStudents[i] !== 'string') { throw new TypeError('Students must be an array of strings'); }
+    }
+    this._students = newStudents;
+  }
+}
