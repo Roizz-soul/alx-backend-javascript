84bababe3c7513714734995920d476eb091a8e90
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-08-16 10:13:05 +0000

    work

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Interface for Teacher object
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -5,4 +6,51 @@ interface Teacher {
   yearsOfExperience?: number;
   location: string;
   [key: string]: any;
+}
+
+// Interface for Directors inheriting Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Interface for printTeacher function
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+// Function printTeacher
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Interface for the constructor
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Interface for the class
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+// Implementing the StudentClass
+const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
 }
